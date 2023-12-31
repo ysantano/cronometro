7728eb142690fdbba05dec960f15f0e63d1abdf2
ysantano <96537184+ysantano@users.noreply.github.com>
2023-12-31 10:05:01 -0600

    20231231.1004

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
