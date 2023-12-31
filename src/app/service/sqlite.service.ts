6349822f9c828b92da7182af1ecdc49c1332cae3
ysantano <96537184+ysantano@users.noreply.github.com>
2023-12-30 19:10:48 -0600

    20231230.1910

--- src/app/service/sqlite.service.ts
+++ src/app/service/sqlite.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { Song } from './song';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
-import { Platform } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
@@ -14,8 +18,9 @@ export class SqliteService {
   constructor(
     private platform: Platform,
     private sqlite: SQLite,
+    private httpClient: HttpClient,
     private sqlPorter: SQLitePorter
-  ) 
+  )
   {
     this.platform.ready().then(() => {
       this.sqlite
@@ -27,13 +32,90 @@ export class SqliteService {
           this.storage = db;
           this.getFakeData();
         });
-    });    
+    });
   }
 
   dbState() {
     return this.isDbReady.asObservable();
-  }  
+  }
 
-  
-
+  fetchSongs(): Observable<Song[]> {
+    return this.songsList.asObservable();
+  }
+  // Render fake data
+  getFakeData() {
+    this.httpClient
+      .get('assets/dump.sql', { responseType: 'text' })
+      .subscribe((data) => {
+        this.sqlPorter
+          .importSqlToDb(this.storage, data)
+          .then((_) => {
+            this.getSongs();
+            this.isDbReady.next(true);
+          })
+          .catch((error) => console.error(error));
+      });
+  }
+  // Get list
+  getSongs() {
+    return this.storage
+      .executeSql('SELECT * FROM songtable', [])
+      .then((res) => {
+        let items: Song[] = [];
+        if (res.rows.length > 0) {
+          for (var i = 0; i < res.rows.length; i++) {
+            items.push({
+              id: res.rows.item(i).id,
+              artist_name: res.rows.item(i).artist_name,
+              song_name: res.rows.item(i).song_name,
+            });
+          }
+        }
+        this.songsList.next(items);
+      });
+  }
+  // Add
+  addSong(artist_name: any, song_name: any) {
+    let data = [artist_name, song_name];
+    return this.storage
+      .executeSql(
+        'INSERT INTO songtable (artist_name, song_name) VALUES (?, ?)',
+        data
+      )
+      .then((res) => {
+        this.getSongs();
+      });
+  }
+  // Get single object
+  getSong(id: any): Promise<Song> {
+    return this.storage
+      .executeSql('SELECT * FROM songtable WHERE id = ?', [id])
+      .then((res) => {
+        return {
+          id: res.rows.item(0).id,
+          artist_name: res.rows.item(0).artist_name,
+          song_name: res.rows.item(0).song_name,
+        };
+      });
+  }
+  // Update
+  updateSong(id: any, song: Song) {
+    let data = [song.artist_name, song.song_name];
+    return this.storage
+      .executeSql(
+        `UPDATE songtable SET artist_name = ?, song_name = ? WHERE id = ${id}`,
+        data
+      )
+      .then((data) => {
+        this.getSongs();
+      });
+  }
+  // Delete
+  deleteSong(id: any) {
+    return this.storage
+      .executeSql('DELETE FROM songtable WHERE id = ?', [id])
+      .then((_) => {
+        this.getSongs();
+      });
+  }
 }
