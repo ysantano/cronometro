3a142f3128a3819d0c2b4975057fb73bbbed992e
ysantano <96537184+ysantano@users.noreply.github.com>
2024-01-03 07:50:23 -0600

    20240103.0750

--- src/app/service/storage.service.ts
+++ src/app/service/storage.service.ts
@@ -17,8 +17,34 @@ export class StorageService {
     this._storage = storage;
   }
 
-  public set(key: string, value: any) {
-    this._storage?.set(key, value);
+  public async set(key: string, value: any) {
+    const result = await this._storage?.set(key, value);
+  }
+
+  public async get(key: string) {
+    const result = await this._storage?.get(key);
+  }
+
+  public async remove(key: string) {
+    const result = await this._storage?.remove(key);
+  }
+
+  public async clear() {
+    const result = await this._storage?.clear();
+  }
+
+  public async keys() {
+    const result = await this._storage?.keys()
+  }
+
+  public async length() {
+    const result = await this._storage?.length()
+  }
+
+  public async list() {
+    this._storage?.forEach((key, value, index) => {
+      console.log('key: ' + key + ' value: ' + value + ' index: ' + index);
+    });
   }
 
 }
