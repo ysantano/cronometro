1d7c575d251bb99be80cffb33bc9b5830bf78233
ysantano <96537184+ysantano@users.noreply.github.com>
2024-02-29 23:02:53 -0600

    20240229_2301

--- src/app/pages/croconfig/croconfig.page.ts
+++ src/app/pages/croconfig/croconfig.page.ts
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CroconfigPage implements OnInit {
 
+  timeTab: string = "T1";
+  minutosInputT1: number = 21;
+  segundosInputT1: number = 1;
+  minutosInputMT: number = 10;
+  segundosInputMT: number = 0;
+  minutosInputT2: number = 22;
+  segundosInputT2: number = 2;
+
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  updateTimeT1() {
+  }
+
+  resetCronometroActivo() {
+  }
+
+  closeModal() {
   }
 
 }
